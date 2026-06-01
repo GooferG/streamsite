21ba7b8a5a305f2453814e555db5b2ab6dc0b868
GooferG <luiz.meneghim@hotmail.com>
2026-05-31 21:15:03 -0700

    feat: add ThemeSwitcher chips and wire theme to URL param

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -2,6 +2,7 @@ import { useSearchParams } from 'react-router-dom';
 import { useLeaderboardData } from '../../hooks/useLeaderboardData';
 import useNow from './useNow';
 import { THEMES, DEFAULT_THEME_ID } from './themes';
+import ThemeSwitcher from './ThemeSwitcher';
 
 function resolveTheme(requestedId) {
   return THEMES.find((t) => t.id === requestedId) || THEMES[0];
@@ -10,12 +11,37 @@ function resolveTheme(requestedId) {
 export default function Leaderboard() {
   const data = useLeaderboardData();
   const now = useNow();
-  const [params] = useSearchParams();
+  const [params, setParams] = useSearchParams();
 
   const active = resolveTheme(params.get('theme'));
   const ActiveTheme = active.Component;
 
-  return <ActiveTheme data={data} now={now} />;
+  const handleSelect = (id) => {
+    setParams(
+      (prev) => {
+        const next = new URLSearchParams(prev);
+        if (id === DEFAULT_THEME_ID) {
+          next.delete('theme');
+        } else {
+          next.set('theme', id);
+        }
+        return next;
+      },
+      { replace: true },
+    );
+  };
+
+  return (
+    <div>
+      <ThemeSwitcher
+        themes={THEMES}
+        activeId={active.id}
+        defaultId={DEFAULT_THEME_ID}
+        onSelect={handleSelect}
+      />
+      <ActiveTheme data={data} now={now} />
+    </div>
+  );
 }
 
 export { DEFAULT_THEME_ID };

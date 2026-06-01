82ed060f803a0533b11f5c930773686ded0e1a14
GooferG <luiz.meneghim@hotmail.com>
2026-05-31 21:03:21 -0700

    feat: add leaderboard theme registry with broadcast

---
+++ src/components/Leaderboard/themes/index.js
@@ -0,0 +1,7 @@
+import BroadcastTheme from './BroadcastTheme';
+
+export const DEFAULT_THEME_ID = 'broadcast';
+
+export const THEMES = [
+  { id: 'broadcast', label: 'Broadcast', Component: BroadcastTheme },
+];
